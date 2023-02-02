faf182af755b02864544c1298cc08d2113ec72ca
Magda K-L <magdalenazur86@gmail.com>
2023-02-03 00:40:06 +0100

    user service methods added, user class added

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,3 +1,27 @@
 export class User {
 
+  public id: number;
+  public userIdNumber: number;
+  public firstName: string ;
+  public lastName: string ;
+  public username: string ;
+  public email: string;
+  public password: string;
+  public lastLoginDisplay: Date;
+  public registrationDate: Date;
+  public isActive: boolean;
+  public isNonLocked: boolean;
+  public rolePermissions: string;
+  public authorities: [];
+
+  constructor() {
+    this.firstName = '';
+    this.lastName = '';
+    this.username = '';
+    this.email = '';
+    this.isActive = false;
+    this.isNonLocked = false;
+    this.rolePermissions = '';
+    this.authorities = [];
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { environment} from "../../environments/environment";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../model/user";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private localhost = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public saveUser(formData: FormData): Observable<User | HttpErrorResponse>{
+    return this.http.post<User>(`${this.localhost}/user/add`, formData);
+  }
+
+  public updateUser(formData: FormData): Observable<User | HttpErrorResponse>{
+    return this.http.post<User>(`${this.localhost}/user/update`, formData);
+  }
+
+  public getAllUsers(): Observable<User[] | HttpErrorResponse>{
+    return this.http.get<User[]>(`${this.localhost}/user/list`);
+  }
+
+  public deleteUser(id: number): Observable<any | HttpErrorResponse>{
+    return this.http.delete<any>(`${this.localhost}/user/delete/${id}`);
+  }
+
+  public addUsersToCache(users: User[]): void{
+    localStorage.setItem('users', JSON.stringify(users));
+  }
+
+  public getUsersFromCache(): User[]{
+    if(localStorage.getItem('users')){
+      return JSON.parse(localStorage.getItem('users'));
+    }
+    return null;
+  }
+
 }
